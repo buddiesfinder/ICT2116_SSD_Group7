9dcd77ae9a06c2576c3b2aadf0a94c7a1fac0194
Daniel <70926662+Sacred29@users.noreply.github.com>
2025-05-17 12:52:42 +0800

    Redis connection

---
+++ lib/redis.ts
@@ -0,0 +1,11 @@
+// lib/redis.ts
+import Redis from "ioredis";
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,       // e.g., "127.0.0.1"
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD, // if any
+//   username: process.env.REDIS_USERNAME, // optional
+});
+
+export default redis;
