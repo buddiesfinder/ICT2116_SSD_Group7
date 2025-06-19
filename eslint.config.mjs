3afc2373db00434978c4d537cf871b629e37f13c
jamestanzr <59002639+jamestanzr@users.noreply.github.com>
2025-06-19 16:16:37 +0800

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      // Relaxed rule settings
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "react/no-unescaped-entities": "warn",
+      "@next/next/no-img-element": "warn",
+      "@next/next/no-html-link-for-pages": "warn"
+    }
+  }
 ];
 
 export default eslintConfig;
