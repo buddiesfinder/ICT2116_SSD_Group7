247d14a0118c79e09c20081c34226a67f2929c92
Oon Jun Jie Marvin <2302045@sit.singaporetech.edu.sg>
2025-07-01 00:12:53 +0800

    Removed line for layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ export const metadata: Metadata = {
   description: 'Login and Register Demo',
 };
 
-export const dynamic = 'force-dynamic';
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const cookieStore = await cookies();
