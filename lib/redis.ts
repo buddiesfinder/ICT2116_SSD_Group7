574c8ecd613461fc1491e5abe8249a5c75a7dec9
lzj1899 <145450814+lzj1899@users.noreply.github.com>
2025-07-01 20:54:45 +0800

    Update Redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -5,7 +5,7 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,       // e.g., "127.0.0.1"
   port: Number(process.env.REDIS_PORT) || 6379,
   password: process.env.REDIS_PASSWORD, // if any
-//   username: process.env.REDIS_USERNAME, // optional
+  username: process.env.REDIS_USER, // optional
 });
 
 export default redis;
