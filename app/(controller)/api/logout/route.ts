915c17fe35caa9f7fbd2ce17429028db5a129deb
Oon Jun Jie Marvin <2302045@sit.singaporetech.edu.sg>
2025-07-01 13:58:35 +0800

    update logout route.ts

--- app/(controller)/api/logout/route.ts
+++ app/(controller)/api/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function POST() {
-  const response = NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_BASE_URL || '/'));
+  const response = NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_BASE_URL || ''));
 
   // Clear the session cookie
   response.cookies.set('refresh_token', '', {
